f045cccbad1403d72adf16ccc4462581ea822f01
Andre Pina Dos Santos <5pinaa28@solent.ac.uk>
2021-12-02 16:15:26 +0000

    added code to visual

--- visual.py
+++ visual.py
@@ -47,6 +47,18 @@ def observation_chart(records):
     plt.show()
 
 
-def animated_summary(records):
+fig, ax = plt.subplots()
 
-    pass
+
+def animate(frame):
+    global ax
+    ax.cla()
+    ax.set_xlim(0, 10)
+    ax.set_ylim(0, 10)
+    ax.plot(frame, frame, 'ro')
+
+
+def animated_summary(records):
+    global fig
+    simple_animation = animation.FuncAnimation(fig, animate, frames=10, interval=1000)
+    plt.show()
